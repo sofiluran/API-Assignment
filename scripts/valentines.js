c9aa25d929a1e19ef4d4e797ad2f763c0817be35
Clara Swahn <clara.swahn@gmail.com>
2025-12-17 00:33:02 +0100

    add fetch functionality

--- scripts/valentines.js
+++ scripts/valentines.js
@@ -1,30 +1,58 @@
-const apiKey = "d2db7f69"
-const apiUrl = "https://www.omdbapi.com/"
+const valentinesUrl = "https://valentines-api.vercel.app/movies"
 const searchInput = document.getElementById("search-input")
 const result = document.querySelector(".main")
+const randomButton = document.getElementById("button-random")
+let remainingMovies = []
 
+const initializeMovies = async () => {
+  try {
+    const response = await fetch(valentinesUrl)
+    if (!response.ok) throw new Error("Could not fetch API")
+    remainingMovies = await response.json()
+  } catch (error) {
+    result.textContent = error.message;
+  }
+}
+
+const callValentinesMovie = () => {
+  if (remainingMovies.length === 0) {
+    result.textContent = "There are no more movies in the Top 50 list!"
+    randomButton.disabled = true
+    return
+  }
+
+  const randomIndex = Math.floor(Math.random() * remainingMovies.length)
+  const randomMovie = remainingMovies[randomIndex]
+
+  remainingMovies.splice(randomIndex, 1)
+
+  displayResult(randomMovie)
+}
+
+randomButton.addEventListener("click", callValentinesMovie)
 
 const searchMovie = async () => {
+  const searchValue = searchInput.value.trim().toLowerCase()
 
-  const searchValue = searchInput.value.trim()
-  const url = `${apiUrl}?apikey=${apiKey}&t=${searchValue}`
+  if (!searchValue) {
+    result.innerHTML = "NOTHING HERE"
+    return
+  }
 
   try {
-    const response = await (fetch(url))
-
+    const response = await fetch(valentinesUrl)
     if (!response.ok) {
       throw new Error("Network not ok")
     }
 
-    const movie = await response.json()
+    const movies = await response.json()
+    const foundMovie = movies.find(movie => movie.title.toLowerCase().includes(searchValue))
 
-    if (movie.Response === "True") {
-      console.log("Movie", movie)
-      displayResult(movie)
+    if (foundMovie) {
+      displayResult(foundMovie)
     } else {
-      result.textContent = ""
+      result.innerHTML = "This movie is not in our Top 50 Halloween-list!"
     }
-
   } catch (error) {
     result.textContent = error
   }
@@ -39,13 +67,14 @@ searchInput.addEventListener("input", () => {
 const displayResult = (movie) => {
   result.innerHTML = ""
 
-  const title = movie.Title
-  const year = movie.Year
-  const poster = movie.Poster
-  const runtime = movie.Runtime
-  const rating = movie.Rated
-  const plot = movie.Plot
-  const imdbRating = movie.imdbRating
+  const title = movie.title
+  const year = movie.release_year
+  const poster = movie.poster;
+  const runtime = movie.runtime
+  const rating = movie.rating
+  const description = movie.description
+  const imdbRating = movie.imdb_rating
+  const link = movie.url
 
   const movieDiv = document.createElement("div")
   movieDiv.classList.add("movie-container")
@@ -55,18 +84,27 @@ const displayResult = (movie) => {
   ${poster !== "N/A" ? `<img src="${poster}" alt="${title} poster">`
       : `<div></div>`
     } 
-    <div class="movie-info">
-      <h2> <b>${title}</b> </h2>
-      <div class="movie-stats">
-        <h3> <b>Year</b> <br><br> ${year} </h3>
-        <h3> <b>Imdb Rating</b> <br><br> ${imdbRating} </h3>
-        <h3> <b>Runtime</b> <br><br> ${runtime} </h3>
-        <h3> <b>MPA</b> <br><br> ${rating} </h3>
-      </div>
-      <h3> ${plot} </h3>
-      <br>
-      <button id="button-random">Read more on IMDB</button>
+    <h2 class="movie-title info"> ${title} </h2>
+    <div class="info"> 
+    <h3>Year:</h3>
+    <p>${year}</p>
     </div>
+    <div class="info"> 
+    <h3> Imdb Rating:</h3>
+    <p>${imdbRating}</p>
+    </div>
+    <div class="info"> 
+    <h3> Runtime:</h3>
+    <p> ${runtime} </p>
+    </div>
+    <div class="info"> 
+    <h3> MPA Rating:</h3>
+    <p>${rating}</p>
+    </div>
+    <p class="movie-plot"> ${description} </p>
+    <a href="${link}" id="button-random", class="imdb-button">Read more on IMDB</a>
   `
   result.appendChild(movieDiv)
 }
+
+initializeMovies()
